b3e9d077346ef8a3450fdd1a1314127f07ceafc2
1102056洪慈惠 <96213059+tzuhui666@users.noreply.github.com>
2024-04-22 14:20:08 +0800

    Add files via upload

---
+++ 北斗七星/1102056_HW1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace _1102056_HW1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openGLControl1 = new Tao.Platform.Windows.OpenGLControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openGLControl1
+            // 
+            this.openGLControl1.AccumBits = ((byte)(0));
+            this.openGLControl1.AutoCheckErrors = false;
+            this.openGLControl1.AutoFinish = false;
+            this.openGLControl1.AutoMakeCurrent = true;
+            this.openGLControl1.AutoSwapBuffers = true;
+            this.openGLControl1.BackColor = System.Drawing.Color.Black;
+            this.openGLControl1.ColorBits = ((byte)(32));
+            this.openGLControl1.DepthBits = ((byte)(16));
+            this.openGLControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.openGLControl1.Location = new System.Drawing.Point(0, 0);
+            this.openGLControl1.Name = "openGLControl1";
+            this.openGLControl1.Size = new System.Drawing.Size(800, 450);
+            this.openGLControl1.StencilBits = ((byte)(0));
+            this.openGLControl1.TabIndex = 0;
+            this.openGLControl1.Load += new System.EventHandler(this.openGLControl1_Load);
+            this.openGLControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.openGLControl1_Paint);
+            this.openGLControl1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.openGLControl1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(270, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "平移量";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(450, 413);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "?????";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.openGLControl1);
+            this.Name = "Form1";
+            this.Text = "北斗七星";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.OpenGLControl openGLControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 北斗七星/1102056_HW1/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tao.OpenGl;
+using Tao.FreeGlut;
+
+namespace _1102056_HW1
+{
+    public partial class Form1 : Form
+    {
+        int x = 0, y = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            this.openGLControl1.InitializeContexts();
+            MessageBox.Show("可以按WSAD的上下左右移動北斗七星喔!");
+        }
+
+        private void openGLControl1_Load(object sender, EventArgs e)
+        {
+            Gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);  //設定清除畫面的-顏色-
+
+            Gl.glMatrixMode(Gl.GL_PROJECTION); //告知系統目前要更改的是-投影矩陣-
+            Gl.glLoadIdentity(); //將目前矩陣設為-單位矩陣Identity-(即初始化)
+            Glu.gluOrtho2D(0.0f, this.openGLControl1.Size.Width, 0.0f, this.openGLControl1.Size.Height); //可設定繪圖範圍(left,right,bottom,top)
+            //物體如果在範圍外不會顯示
+        }
+
+        private void openGLControl1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.D:
+                    x += 5;
+                    break;
+                case Keys.A:
+                    x += -5;
+                    break;
+                case Keys.W:
+                    y += 5;
+                    break;
+                case Keys.S:
+                    y += -5;
+                    break;
+            }
+            this.openGLControl1.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "左右平移" + x + "上下平移" + y;
+        }
+
+        private void openGLControl1_Paint(object sender, PaintEventArgs e)
+        {
+            //繪製小星星
+            Random rn = new Random(0); //亂數產生器  裡面可放種子  一樣亂數序列就會相同
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT); //清除畫面 將色彩緩衝器全部清除
+            Gl.glPointSize(1.0f);
+            Gl.glColor3ub(255,255,255);
+            Gl.glBegin(Gl.GL_POINTS);
+            for (int i = 0; i < 200; i++)  //會有200星星
+            {
+                Gl.glVertex2i(rn.Next(0, this.openGLControl1.Size.Width),
+                rn.Next(0, this.openGLControl1.Size.Height));//0-799/0-449之間隨機產生
+            }
+            Gl.glEnd();
+
+
+            Gl.glPointSize(4.0f);//設定北斗七星大小//不能放在begin/end中間
+
+
+            //繪製北斗七星
+            Gl.glBegin(Gl.GL_POINTS);//成對出現
+
+
+            Gl.glColor3ub((byte)255, (byte)0, (byte)0);//red
+            Gl.glVertex2i(320 + x, 200 + y);//設定位置
+            Gl.glColor3ub((byte)255, (byte)128, (byte)0);//orange
+            Gl.glVertex2i(340 + x, 160 + y);
+            Gl.glColor3ub((byte)255, (byte)255, (byte)0);//yellow
+            Gl.glVertex2i(295 + x, 110 + y);
+            Gl.glColor3ub((byte)0, (byte)128, (byte)0);//green
+            Gl.glVertex2i(260 + x, 135 + y);
+            Gl.glColor3ub((byte)0, (byte)0, (byte)255);//blue
+            Gl.glVertex2i(210 + x, 120 + y);
+            Gl.glColor3ub((byte)255, (byte)0, (byte)255);//pink
+            Gl.glVertex2i(165 + x, 110 + y);
+            Gl.glColor3ub((byte)128, (byte)0, (byte)128);//purple
+            Gl.glVertex2i(120 + x, 50 + y);
+
+
+            Gl.glEnd();//成對出現
+
+            
+
+
+        }
+    }
+}
